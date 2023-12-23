0dd074338f5f320a822b4294e989b884b914ed87
Ahmed <ahmedbouterfa20@gmail.com>
2023-12-23 13:44:11 +0100

    vergen 3

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+
+function Home() {
+  
+  return (
+    <main >
+      <h1>vergen 3</h1>
+      <h1>hello My friend</h1>
+      <h1 className="css">Project Counter</h1>
+      <hr />
+      <h2>Hello World</h2>
+      <div className="container">
+        <h3>ahmed academy</h3>
+        <h3>ahmed web developer</h3>
+      </div>
+      
+    </main>
+  );
+}
+
+export default Home;
